8eb7181ccfda558262d19140ab0faba51e8f74ac
Jorge <jdiazort@student02.cse.nd.edu>
2016-04-29 18:29:15 -0400

    Jorge

--- Pi/serial.cpp
+++ Pi/serial.cpp
@@ -1,10 +1,18 @@
 #include "serial.hpp"
+#ifdef LOG1
+#include "../loggerTesting/log1.h"
+#else
+#include "../loggerTesting/log.h"
+#endif
 
 // contructor, initializes serial port using boost
 Serial::Serial(std::string port, unsigned int baud) :
   io(),	// needed for ...
   serial(io, port)
 {
+  FILELog::ReportingLevel() = FILELog::FromString(/*argv[1] ? argv[1] : */"DEBUG1"); // initializing logger
+  FILE_LOG(logDEBUG) << "Initializing Serial logger." ;
+
   serial.set_option(boost::asio::serial_port_base::baud_rate(baud));
   serial.set_option(boost::asio::serial_port_base::character_size(8));
   serial.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
@@ -26,14 +34,14 @@ char Serial::getChar() {
 
 // returns string read from buffer up until the specified delimiter
 std::string Serial::readUntil(char delim) {
-  std::cout << "Attempting read_until..." << std::endl;
+  FILE_LOG(logDEBUG) << "Attempting read_until...";
   boost::asio::read_until(serial, buffer, delim);
-  std::cout << "Done read_until\n"
-            << "Attempting string conversion..." << std::endl;
+  FILE_LOG(logDEBUG) << "Done read_until.";
+  FILE_LOG(logDEBUG) << "Attempting string conversion...";
   std::istream is(&buffer);
   std::string target;
   std::getline(is, target, delim);
-  std::cout << "Done with string conversion, returning" << std::endl;
+  FILE_LOG(logDEBUG) << "Done with string conversion, returning.";
   return target;
 }
 
--- Pi/user.cpp
+++ Pi/user.cpp
@@ -1,17 +1,33 @@
 #include "user.hpp"
+#ifdef LOG1
+#include "../loggerTesting/log1.h"
+#else
+#include "../loggerTesting/log.h"
+#endif
 
+/* Simple User class which stores the name of a particular user. 
+*/
+
+// constructor
 User::User() {
 }
 
+// constructor, sets the name of the user to what is passed in as a parameter
 User::User(std::string name) :
   name(name)
 {
+  FILELog::ReportingLevel() = FILELog::FromString(/*argv[1] ? argv[1] : */"DEBUG1");
+  FILE_LOG(logDEBUG) << "Initializing user logger.";
+  FILE_LOG(logDEBUG) << "Setting user name to '" << name << "'.";
+
 }
 
+// returns private data member name
 std::string User::getName() {
   return name;
 }
 
+// serts name to that passed in as a parameter 
 void User::setName(std::string n) {
   name = n;
 }
